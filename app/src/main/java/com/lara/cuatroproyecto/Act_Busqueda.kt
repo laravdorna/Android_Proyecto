b38d389af1e4ec67ed219d94b7dde50da633f739
laravdorna <lvazquezdorna@danielcastelao.org>
2019-12-09 10:28:51 +0100

    4.toast bussqueda

--- app/src/main/java/com/lara/cuatroproyecto/Act_Busqueda.kt
+++ app/src/main/java/com/lara/cuatroproyecto/Act_Busqueda.kt
@@ -2,6 +2,9 @@ package com.lara.cuatroproyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_act__busqueda.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.toast
 
 class Act_Busqueda : AppCompatActivity() {
 
@@ -11,6 +14,37 @@ class Act_Busqueda : AppCompatActivity() {
 
 
 
+        btn_comprobar.setOnClickListener {
+            comprobar()
+        }
+
+        //boton que llama una url con una pagina pista para la respuesta
+        btn_pista.setOnClickListener {
+            browse("https://es.wikipedia.org/wiki/Computadora_personal#Historia")
+        }
+
 
     }
+
+
+    //funcion que comprueba que la fecha sea la correcta
+    fun comprobar() {
+
+        val fechaCorrecta = 1964
+        //meter la fecha dada en un int
+        val fecha = Integer.parseInt(txt_fecha.toString())
+
+
+
+        if (fecha == fechaCorrecta) {
+            toast("RESPUESTA CORRECTA")
+
+
+        } else {
+            toast("INTENTALO DE NUEVO")
+
+        }
+
+    }
+
 }
