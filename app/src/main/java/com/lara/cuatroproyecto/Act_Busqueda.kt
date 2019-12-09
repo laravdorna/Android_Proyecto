0abe5f8264c94327f0521d5df533f0356e984889
laravdorna <lvazquezdorna@danielcastelao.org>
2019-12-09 09:36:40 +0100

    2.Botones act busqueda

---
+++ app/src/main/java/com/lara/cuatroproyecto/Act_Busqueda.kt
@@ -0,0 +1,12 @@
+package com.lara.cuatroproyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Act_Busqueda : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act__busqueda)
+    }
+}
